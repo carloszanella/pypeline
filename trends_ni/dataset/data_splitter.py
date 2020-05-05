f6c1d57bd72b628fe54d94868e3b24bdb9b5bddb
carloszanella <cfszanella@gmail.com>
2020-05-05 11:20:31 +0200

    Update splitter to work with numpy  arrays
    Took 7 minutes

--- trends_ni/dataset/data_splitter.py
+++ trends_ni/dataset/data_splitter.py
@@ -4,13 +4,13 @@ import numpy as np
 
 
 class TrainValSplitter:
-    def __init__(self, train_ids: List[float]):
-        self.train_ids = train_ids
-        self.ds_size = len(train_ids)
+    def __init__(self, ids: np.array):
+        self.ids = ids
+        self.ds_size = len(ids)
 
     def split(self, val_split: float = 0.2) -> Tuple[np.array, np.array]:
         val_size = int(np.floor(val_split * self.ds_size))
-        val_ids = np.random.choice(self.train_ids, size=val_size, replace=False)
-        train_ids = self.train_ids.drop(val_ids)
+        val_ids = np.random.choice(self.ids, size=val_size, replace=False)
+        train_ids = np.delete(self.ids, val_ids)
 
         return train_ids, val_ids
--- trends_ni/test/test_dataset_splitter.py
+++ trends_ni/test/test_dataset_splitter.py
@@ -1,12 +1,12 @@
 from trends_ni.dataset.data_splitter import TrainValSplitter
-from trends_ni.util import get_train_ids
+import numpy as np
 
 
 def test_train_val_splitter():
-    train_ids = get_train_ids()
-    train_splitter = TrainValSplitter(train_ids)
+    ids = np.arange(1, 100, 50)
+    train_splitter = TrainValSplitter(ids)
     val_split = 0.2
     train_ix, val_ix = train_splitter.split(val_split)
 
-    assert len(val_ix) + len(train_ix) == len(train_ids)
-    assert int(val_split * len(train_ids)) == len(val_ix)
+    assert len(val_ix) + len(train_ix) == len(ids)
+    assert int(val_split * len(ids)) == len(val_ix)
