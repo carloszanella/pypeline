3c557fddc7ce04e2c6c1ae158ca632e2dcd95c79
carloszanella <cfszanella@gmail.com>
2020-05-05 19:12:50 +0200

    Remove attributes from trainvalsplitter class and move to method
    Took 29 seconds

--- trends_ni/dataset/data_splitter.py
+++ trends_ni/dataset/data_splitter.py
@@ -3,14 +3,16 @@ from typing import List, Tuple
 import numpy as np
 
 
-class TrainValSplitter:
-    def __init__(self, ids: np.array):
-        self.ids = ids
-        self.ds_size = len(ids)
+class DataSplitter:
+    def split(self, ids: np.array, val_split: float) -> Tuple[np.array, np.array]:
+        pass
 
-    def split(self, val_split: float = 0.2) -> Tuple[np.array, np.array]:
-        val_size = int(np.floor(val_split * self.ds_size))
-        val_ids = np.random.choice(self.ids, size=val_size, replace=False)
-        train_ids = np.delete(self.ids, val_ids)
+
+class TrainValSplitter(DataSplitter):
+    def split(self, ids: np.array, val_split: float = 0.2) -> Tuple[np.array, np.array]:
+        ds_size = len(ids)
+        val_size = int(np.floor(val_split * ds_size))
+        val_ids = np.random.choice(ids, size=val_size, replace=False)
+        train_ids = np.delete(ids, val_ids)
 
         return train_ids, val_ids
--- trends_ni/test/test_dataset_splitter.py
+++ trends_ni/test/test_dataset_splitter.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def test_train_val_splitter():
     ids = np.arange(1, 100, 50)
-    train_splitter = TrainValSplitter(ids)
+    train_splitter = TrainValSplitter()
     val_split = 0.2
-    train_ix, val_ix = train_splitter.split(val_split)
+    train_ix, val_ix = train_splitter.split(ids, val_split)
 
     assert len(val_ix) + len(train_ix) == len(ids)
     assert int(val_split * len(ids)) == len(val_ix)
