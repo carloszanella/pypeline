1d4da4a86d8316a47aa0b7ecd8c9cefe48b85a05
carloszanella <cfszanella@gmail.com>
2020-05-10 17:07:48 +0200

    Add util function to make submissions out of predictions
    Took 43 minutes

--- trends_ni/test/test_util.py
+++ trends_ni/test/test_util.py
@@ -1,6 +1,12 @@
-from trends_ni.util import get_train_ids
+from trends_ni.util import get_train_ids, make_submission
 
 
 def test_get_train_ids():
     ids = get_train_ids()
     assert ids.all()
+
+
+def test_make_submission(sample_predictions, sample_ids):
+    sub = make_submission(sample_predictions, sample_ids)
+    assert sub.all().all()
+    assert sub.shape == (len(sample_ids) * 5, 1)
--- trends_ni/util.py
+++ trends_ni/util.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pandas as pd
 import numpy as np
 
@@ -8,3 +10,18 @@ def get_train_ids() -> np.ndarray:
     train_scores = pd.read_csv(structure.raw.y_train, index_col=0)
     train_ids = train_scores.index.values
     return train_ids
+
+
+def make_submission(predictions: np.ndarray, ids: List[float]) -> pd.DataFrame:
+    columns = pd.Series(["age", "domain1_var1", "domain1_var2", "domain2_var1", "domain2_var2"])
+    piled_preds = predictions.reshape(-1, 1)
+
+    index = []
+    for i in ids:
+        index.append(str(i) + "_" + columns)
+
+    index = pd.concat(index)
+
+    submission = pd.DataFrame(piled_preds, index=index)
+
+    return submission
